1f401543d6bd6930383d40767942403ce84faa9c
epicmine99 <sean.ryan@mail.utoronto.ca>
2023-11-07 16:44:01 -0500

    models for accounts

--- P2/petpal/accounts/models.py
+++ P2/petpal/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class Seeker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    email = models.EmailField()
+    phone_number = PhoneNumberField(null = False, blank = False)
+    description = models.CharField(max_length=600, null=False)
+    address = models.CharField(max_length=200, null=False)
+    age = models.IntegerField(null=True)
+
+
+class Shelter(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=False, null = False) 
+    phone_number = PhoneNumberField(null = False, blank = False)
+    description = models.CharField(max_length=600, null=False)
+    address = models.CharField(max_length=200, null=False, blank=True)
+    website = models.URLField(max_length=128, blank=True)
