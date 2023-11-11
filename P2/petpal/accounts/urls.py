71d2fa50a98e2c200541b66d341456fe63802673
epicmine99 <sean.ryan@mail.utoronto.ca>
2023-11-11 17:11:12 -0500

    ddd

--- P2/petpal/accounts/urls.py
+++ P2/petpal/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import SeekerCreate, ShelterCreate
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("seekers/create", SeekerCreate.as_view(), name="seeker-list-create"),
     path("shelters/create", ShelterCreate.as_view(), name="shelter-list-create"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- P2/petpal/comment/models.py
+++ P2/petpal/comment/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from accounts.models import Shelter, Seeker
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 # assuming that the shelter model is already created in the accounts app
 # Create your models here.
+
 
 class Comment(models.Model):
     # shelter_id = models.ForeignKey(Shelter, on_delete=models.CASCADE)
@@ -10,9 +12,12 @@ class Comment(models.Model):
 
     shelter_id = models.IntegerField(default=1, blank=False, null=False)
     seeker_id = models.IntegerField(default=1, blank=False, null=False)
-    rating = models.IntegerField(default=1, 
-                                 validators=[MaxValueValidator(5), MinValueValidator(1)],
-                                 blank=False, null=False)
+    rating = models.IntegerField(
+        default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)],
+        blank=False,
+        null=False,
+    )
     content = models.TextField(default="")
     date = models.DateTimeField(auto_now_add=True)
 
