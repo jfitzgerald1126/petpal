fa42247feebc80e163949dff8e0a6ed6af2c0356
epicmine99 <sean.ryan@mail.utoronto.ca>
2023-11-11 19:52:28 -0500

    fixzes

--- P2/petpal/accounts/serializers.py
+++ P2/petpal/accounts/serializers.py
@@ -1,16 +1,80 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Seeker, Shelter
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 
 class SeekerSerializer(ModelSerializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+    confirm_password = serializers.CharField(
+        write_only=True, style={"input_type": "password"}
+    )
+
     class Meta:
         model = Seeker
         fields = "__all__"
-        # serialize the Seeker model as json
+
+    def validate(self, data):
+        # Validate that passwords match
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError("Passwords do not match.")
+
+        try:
+            validate_password(data["password"], self.instance)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+
+        return data
+
+    def create(self, validated_data):
+        user_data = {
+            "username": validated_data.pop("username"),
+            "password": validated_data.pop("password"),
+        }
+
+        # Create a new user
+        user = User.objects.create_user(**user_data)
+
+        seeker = Seeker.objects.create(user=user, **validated_data)
+
+        return seeker
 
 
 class ShelterSerializer(ModelSerializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+    confirm_password = serializers.CharField(
+        write_only=True, style={"input_type": "password"}
+    )
+
     class Meta:
         model = Shelter
         fields = "__all__"
-        # serialize the Seeker model as json
+
+    def validate(self, data):
+        # Validate that passwords match
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError("Passwords do not match.")
+
+        try:
+            validate_password(data["password"], self.instance)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+
+        return data
+
+    def create(self, validated_data):
+        user_data = {
+            "username": validated_data.pop("username"),
+            "password": validated_data.pop("password"),
+        }
+
+        # Create a new user
+        user = User.objects.create_user(**user_data)
+
+        shelter = Shelter.objects.create(user=user, **validated_data)
+
+        return shelter
--- P2/petpal/accounts/views.py
+++ P2/petpal/accounts/views.py
@@ -3,43 +3,30 @@ from rest_framework.generics import ListCreateAPIView
 from .models import Seeker, Shelter
 from .serializers import SeekerSerializer, ShelterSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.permissions import AllowAny
 
 
 # Create your views here.
 
 
-class SeekerCreate(ListCreateAPIView):
+class SeekerListCreateView(generics.ListCreateAPIView):
+    queryset = Seeker.objects.all()
+    serializer_class = SeekerSerializer
+    permission_classes = [AllowAny]
+
+
+class SeekerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Seeker.objects.all()
     serializer_class = SeekerSerializer
 
-    def create(self, request, serializer):
-        username = request.data.get("username")
-        if Seeker.objects.filter(username=username).exists():
-            # username already exists
-            return Response(
-                {"error": "Seeker with this username already exists."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
 
-        # password checks here?
-
-        serializer.save(data=request.data)
-
-
-class ShelterCreate(ListCreateAPIView):
+class ShelterListCreateView(generics.ListCreateAPIView):
     queryset = Shelter.objects.all()
     serializer_class = ShelterSerializer
+    permission_classes = [AllowAny]
 
-    def create(self, request, serializer):
-        username = request.data.get("username")
-        if Shelter.objects.filter(username=username).exists():
-            # username already exists
-            return Response(
-                {"error": "Shelter with this username already exists."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
 
-        # password checks here?
-
-        serializer.save(data=request.data)
+class ShelterRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Shelter.objects.all()
+    serializer_class = ShelterSerializer
