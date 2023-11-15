9b5698a2e46d96610bfa9367aca8e1c2392fc71c
john <jfitzgerald1126@gmail.com>
2023-11-15 16:20:24 -0500

    change perms

--- P2/startup.sh
+++ P2/startup.sh
@@ -2,6 +2,9 @@ sudo apt install python3.8 python3-pip python3.8-venv
 
 python3 -m venv env
 
+chmod a+rwx ./run.sh
+chmod a+rwx ./petpal/manage.py
+
 source ./env/bin/activate
 
 pip install -r ./requirements.txt
