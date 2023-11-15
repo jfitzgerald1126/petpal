978d404c4ab96bdbee803d9015493c15aed3f476
Eshwara Chock <eshchock1@gmail.com>
2023-11-15 15:48:05 -0500

    sh files changed

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,6 +1,4 @@
-PROJECT_DIR="P2/petpal"
-
-sudo apt-get install python3.8 python3-pip
+sudo apt install python3.8 python3-pip
 
 python -m venv env
 
@@ -8,7 +6,5 @@ source env/bin/activate
 
 pip install -r requirements.txt
 
-python manage.py make migrations
-python manage.py migrate
-
-deactivate
+python ./petpal/manage.py makemigrations
+python ./petpal/manage.py migrate
