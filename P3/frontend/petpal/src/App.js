6c851090e1bf88a4f75de96dd8aa92b6564f1496
NMacasaet <nicholas.macasaet@mail.utoronto.ca>
2023-11-29 16:10:16 -0500

    added routes

--- P3/frontend/petpal/src/App.js
+++ P3/frontend/petpal/src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import TestPage from './pages/LoginAndRegister/test';
+
+
+function WebpageRouter(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+            <Route index element={<TestPage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <WebpageRouter/>
   );
 }
 
---
+++ P3/frontend/petpal/src/pages/LoginAndRegister/test.jsx
@@ -0,0 +1,10 @@
+
+
+function TestPage(){
+    
+    return <>
+        <h1> this is just a test to see if the routes are set up correctly </h1>
+    </>;
+}
+
+export default TestPage;
