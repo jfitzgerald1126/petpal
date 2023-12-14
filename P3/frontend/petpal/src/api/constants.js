6bf08517ef9f3c2c886dbc6e3cdfc670068c16fb
john <jfitzgerald1126@gmail.com>
2023-12-14 01:21:47 -0500

    change base url

--- P3/frontend/petpal/src/api/constants.js
+++ P3/frontend/petpal/src/api/constants.js
@@ -1,5 +1,5 @@
 
 
-export const BASE_URL = 'http://127.0.0.1:8000/'
+export const BASE_URL = 'http://3.143.68.230/'
 
 // export const PROD_URL = 'TODO: fill this when we import our project into a prod environment/'
